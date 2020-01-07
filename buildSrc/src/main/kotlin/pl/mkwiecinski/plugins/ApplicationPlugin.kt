30dec9cf1972892cae57d305cc67771a815d19fd
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-01-07 18:02:58 +0100

    refactor: Integrate Android Application Plugin

--- buildSrc/src/main/kotlin/pl/mkwiecinski/plugins/ApplicationPlugin.kt
+++ buildSrc/src/main/kotlin/pl/mkwiecinski/plugins/ApplicationPlugin.kt
@@ -1,10 +1,7 @@
 package pl.mkwiecinski.plugins
 
-import com.android.build.gradle.TestedExtension
-import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import pl.mkwiecinski.plugins.internal.applyDagger
 import pl.mkwiecinski.plugins.internal.applyDaggerAndroid
 import pl.mkwiecinski.plugins.internal.configureUnitTest
@@ -12,40 +9,12 @@ import pl.mkwiecinski.plugins.internal.configureUnitTest
 class ApplicationPlugin : Plugin<Project> {
 
     override fun apply(project: Project): Unit = with(project) {
-        pluginManager.apply("com.android.application")
-        pluginManager.apply("kotlin-android")
-        pluginManager.apply("kotlin-kapt")
-        pluginManager.apply("com.project.starter.quality")
-
-        repositories.google()
-        repositories.jcenter()
+        pluginManager.apply("com.starter.application.android")
+        pluginManager.apply("com.starter.quality")
 
         applyDagger()
         applyDaggerAndroid()
 
         configureUnitTest()
-
-        extensions.getByType(TestedExtension::class.java).apply {
-
-            useLibrary("android.test.runner")
-            useLibrary("android.test.base")
-            useLibrary("android.test.mock")
-
-            configureSourceSets()
-
-            compileOptions.sourceCompatibility = JavaVersion.VERSION_1_8
-            compileOptions.targetCompatibility = JavaVersion.VERSION_1_8
-        }
-
-        tasks.withType(KotlinCompile::class.java).all {
-            it.kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
-        }
-    }
-
-    private fun TestedExtension.configureSourceSets() {
-        sourceSets.all { set ->
-            val withKotlin = set.java.srcDirs.map { it.path.replace("java", "kotlin") }
-            set.java.setSrcDirs(set.java.srcDirs + withKotlin)
-        }
     }
 }
