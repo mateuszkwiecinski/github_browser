e4b1f82f32f239bc95c50186bee339fb4ffe3c91
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2020-03-14 19:59:21 +0100

    wip

--- app/src/main/kotlin/pl/mkwiecinski/browser/MainApplication.kt
+++ app/src/main/kotlin/pl/mkwiecinski/browser/MainApplication.kt
@@ -4,10 +4,13 @@ import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 import pl.mkwiecinski.browser.di.DaggerDomainDependenciesBuilder
 import pl.mkwiecinski.browser.di.DaggerMainComponent
+import pl.mkwiecinski.data.di.NetworkingComponent
 import pl.mkwiecinski.domain.di.DaggerDomainComponent
 import pl.mkwiecinski.domain.di.DomainComponent
+import pl.mkwiecinski.domain.di.DomainDependencies
 import pl.mkwiecinski.presentation.di.DaggerPresentationInComponent
 import pl.mkwiecinski.presentation.di.DaggerPresentationOutComponent
+import pl.mkwiecinski.presentation.di.PresentationOutComponent
 
 internal class MainApplication : DaggerApplication() {
 
@@ -15,12 +18,11 @@ internal class MainApplication : DaggerApplication() {
         val networking = createNetworking()
         val presentationOut = presentationOut()
 
-        val domainDependencies = DaggerDomainDependenciesBuilder.factory().create(
-            networking = networking,
-            presentation = presentationOut
-        )
         val domain = DaggerDomainComponent.factory().create(
-            dependencies = domainDependencies
+            dependencies = domainDependencies(
+                networking = networking,
+                presentationOut = presentationOut
+            )
         )
 
         return DaggerMainComponent.factory().create(
@@ -29,7 +31,13 @@ internal class MainApplication : DaggerApplication() {
         )
     }
 
-    private fun presentationOut() =
+    private fun domainDependencies(networking: NetworkingComponent, presentationOut: PresentationOutComponent): DomainDependencies =
+        DaggerDomainDependenciesBuilder.factory().create(
+            networking = networking,
+            presentation = presentationOut
+        )
+
+    private fun presentationOut(): PresentationOutComponent =
         DaggerPresentationOutComponent.create()
 
     private fun presentationIn(domain: DomainComponent) =
