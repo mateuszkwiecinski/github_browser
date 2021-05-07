16bd08236281df037b3ac249fd2df40358c49cd8
Mateusz Kwieciński <mateusz.kwiatek@gmail.com>
2021-05-07 09:33:32 +0200

    Add custom lint checks

---
+++ custom-lint-checks/src/main/kotlin/IssueRegistry.kt
@@ -0,0 +1,9 @@
+package com.example
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.Issue
+
+class SampleIssueRegistry : IssueRegistry() {
+
+    override val issues = emptyList<Issue>()
+}
